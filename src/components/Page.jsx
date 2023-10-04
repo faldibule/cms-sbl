7a3a8a5aa1230b0332f3383adc73b9d489da11b0
faldibule <faldibule@gmail.com>
2023-10-04 11:21:27 +0700

    Change: item product page

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -10,6 +10,20 @@ const Page = forwardRef(({ children, title = '', meta, ...other }, ref) => (
   <>
     <Helmet>
       <title>{`${title} | Terakom`}</title>
+      <meta name="description" content="undefined" />
+
+      <meta property="og:url" content="https://github.com/theodevoid/v6-academy/blob/main/apps/web/src/components/HeadMetaData.tsx" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title + ' | Terakom'} />
+      <meta property="og:description" content="undefined" />
+      <meta property="og:image" content="" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="github.com" />
+      <meta property="twitter:url" content="https://github.com/theodevoid/v6-academy/blob/main/apps/web/src/components/HeadMetaData.tsx" />
+      <meta name="twitter:title" content={title + ' | Terakom'} />
+      <meta name="twitter:description" content="undefined" />
+      <meta name="twitter:image" content="" />
       {meta}
     </Helmet>
 
--- src/pages/master-data/item-product/index.jsx
+++ src/pages/master-data/item-product/index.jsx
@@ -1,26 +1,68 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { Button, Card, CardContent, Checkbox, Container, Grid, IconButton, InputAdornment, MenuItem, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TextField, Typography } from '@mui/material';
-import Page from '../../../components/Page';
-import CustomSearchComponent from '../../../components/CustomSearchComponent';
+import Page from '@components/Page';
+import CustomSearchComponent from '@components/CustomSearchComponent';
 import { LoadingButton } from '@mui/lab';
 import Loading from '@components/Loading';
-import useFetchItem from '@hooks/item-product/useFetchItem';
-import useDeleteItem from '@hooks/item-product/useDeleteItem';
-import useSaveItem from '@hooks/item-product/useSaveItem';
 import DeleteDialog from '@components/DeleteDialog';
 import CustomActionTableComponent from '@components/CustomActionTableComponent';
-import useFetchItemProduct from '@hooks/item-product/useFetchItem';
-import useDeleteItemProduct from '@hooks/item-product/useDeleteItem';
-import useSaveItemProduct from '@hooks/item-product/useSaveItem';
+import useFetchItemProduct from '@hooks/item-product/useFetchItemProduct';
+import useDeleteItemProduct from '@hooks/item-product/useDeleteItemProduct';
+import useSaveItemProduct from '@hooks/item-product/useSaveItemProduct';
+import useFetchItemCategory from '@hooks/item-category/useFetchItemCategory';
 
 const index = () => {
     const [params, setParams] = useState({
-        page: 0,
+        page: 1,
         limit: 5,
         search: '',
         paginate: 1,
     })
     const { data: rows, refetch, isFetchedAfterMount } = useFetchItemProduct(params)
+
+    // Category & Sub Category
+    const [category, setCategory] = useState('')
+    const { data: itemCategory, isLoading: loadingCategory } = useFetchItemCategory({ paginate: 0 })
+    const handleCategory = (e) => setCategory(e.target.value)
+    const filteredDataParentCategory = useMemo(() => {
+        const temp = itemCategory?.data.filter(v => {
+            return !!!v.parent_category_id
+        })
+        return !!temp ? temp : []
+    }, [itemCategory]) 
+    const filteredDataChildCategory = useMemo(() => {
+        if(category === '') return []
+        const temp = itemCategory?.data.filter(v => {
+            return !!v.parent_category_id && v.parent_category_id === category
+        })
+        return !!temp ? temp : []
+    }, [itemCategory, category]) 
+    const renderInputParentCategory = () => {
+        if(filteredDataParentCategory.length === 0){
+            return (
+                <MenuItem value='' disabled>Kosong</MenuItem>
+            )
+        }
+        return filteredDataParentCategory.map((v) => {
+            return (
+                <MenuItem key={v.id} value={v.id}>{v.category_code} - {v.category}</MenuItem>
+            )
+        })
+    }
+    const renderInputChildCategory = () => {
+        if(filteredDataChildCategory.length === 0){
+            return (
+                <MenuItem value='' disabled>Kosong</MenuItem>
+            )
+        }
+        return filteredDataChildCategory.map((v) => {
+            return (
+                <MenuItem key={v.id} value={v.id}>{v.category_code} - {v.category}</MenuItem>
+            )
+        })
+    }
+
+    // Handle Page
     const handleChangePage = (event, newPage) => {
         setParams((prev) => {
             return {
@@ -39,12 +81,14 @@ const index = () => {
         });
     };
     
+    // Handle Action
     const [loading, setLoading] = useState(false)
     const [staging, setStaging] = useState({})
     const handleReset = (data) => {
         setLoading(true)
         setTimeout(() => {
             setStaging({})
+            setCategory('')
             setLoading(false)
         }, 0);
     }
@@ -56,6 +100,7 @@ const index = () => {
         }, 500);
     }
     
+    // Handle Dialog Delete
     const [open, setOpen] = useState(false)
     const handleClose = (id = null) => {
         if(open){
@@ -65,7 +110,6 @@ const index = () => {
         if(!!!id) return;
         setStaging({ id })
     }
-
     const { mutate: deleteDepartment, isLoading: loadingDelete } = useDeleteItemProduct({
         onSuccess: () => {
             handleReset()
@@ -77,6 +121,7 @@ const index = () => {
         deleteDepartment(staging?.id)
     }
 
+    // HandleSubmit
     const { mutate: save, isLoading: loadingSave, error } = useSaveItemProduct({
         onSuccess: () => {
             handleReset()
@@ -87,13 +132,14 @@ const index = () => {
     const onSubmit = (e) => {
         e.preventDefault()
         const formData = new FormData(e.target)
-        save({ formData, id: staging?.id })
+        console.log(Object.fromEntries(formData))
+        // save({ formData, id: staging?.id })
     }
 
+    // HandleTable
     if(isFetchedAfterMount && params.page !== 1 && rows !== undefined && rows?.data.length === 0){
         setParams({ ...params, page: rows.meta.last_page })
     }
-
     const renderData = () => {
         if(rows === undefined) {
             return (
@@ -182,7 +228,7 @@ const index = () => {
                             </Typography>
                         </Stack>
                     </Grid>
-                    <Grid item xs={12} md={8}>
+                    <Grid item xs={12} md={6}>
                         <Card>
                             <CardContent>
                                 <Grid container spacing={2} sx={{ mb: 2 }} alignItems="center">
@@ -235,81 +281,137 @@ const index = () => {
                             </CardContent>
                         </Card>
                     </Grid>
-                    <Grid item xs={12} md={4}>
+                    <Grid item xs={12} md={6}>
                         <Card sx={{ p: 2 }}>
                             <Typography mb={3} variant='h6'>
                                 {!!staging.id ? 'Form Edit Item Category' : 'Form Add Item Category'}
                             </Typography>
                             {!loading ? 
-                                <Stack rowGap={2} component='form' onSubmit={onSubmit}>
-                                    <TextField
-                                        fullWidth 
-                                        label='Code'
-                                        name='category_code'
-                                        defaultValue={staging?.category_code}
-                                        required
-                                        helperText={!!errors?.category_code && errors?.category_code[0]}
-                                        error={!!errors?.category_code}
-                                    /> 
-                                    <TextField
-                                        fullWidth 
-                                        label='Name'
-                                        name='category'
-                                        defaultValue={staging?.category}
-                                        required
-                                        helperText={!!errors?.category && errors?.category[0]}
-                                        error={!!errors?.category}
-                                    /> 
-                                    <TextField
-                                        fullWidth 
-                                        label='Brand'
-                                        select
-                                    >
-                                        <MenuItem value='1'>Brand 1</MenuItem>
-                                        <MenuItem value='2'>Brand 2</MenuItem>
-                                    </TextField> 
-                                    <TextField
-                                        fullWidth 
-                                        label='Size'
-                                    /> 
-                                    <TextField
-                                        fullWidth 
-                                        label='Unit'
-                                        select
-                                    >
-                                        <MenuItem value=''>None</MenuItem>
-                                        <MenuItem value='kg'>KG</MenuItem>
-                                        <MenuItem value='grm'>GRM</MenuItem>
-                                        <MenuItem value='tin'>TIN</MenuItem>
-                                        <MenuItem value='btl'>BTL</MenuItem>
-                                        <MenuItem value='btl'>LTR</MenuItem>
-                                        <MenuItem value='btl'>TUB</MenuItem>
-                                        <MenuItem value='btl'>BAG</MenuItem>
-                                        <MenuItem value='btl'>EA</MenuItem>
-                                        <MenuItem value='btl'>BOX</MenuItem>
-                                        <MenuItem value='btl'>CTN</MenuItem>
-                                        <MenuItem value='btl'>GLN</MenuItem>
-                                        <MenuItem value='btl'>ROLL</MenuItem>
-                                        <MenuItem value='btl'>SLOP</MenuItem>
-                                        <MenuItem value='btl'>PPN</MenuItem>
-                                        <MenuItem value='btl'>SISIR</MenuItem>
-                                        <MenuItem value='btl'>LOT</MenuItem>
-                                    </TextField> 
-                                    <TextField
-                                        fullWidth 
-                                        label='Tax'
-                                        select
-                                    >
-                                        <MenuItem value='1'>Yes</MenuItem>
-                                        <MenuItem value='2'>No</MenuItem>
-                                    </TextField> 
-                                    <Stack direction='row' spacing={2}>
-                                        <LoadingButton loading={loadingSave} fullWidth variant='contained' type='submit'>
-                                            Submit
-                                        </LoadingButton>
-                                        <Button variant='outlined' onClick={handleReset} fullWidth>Reset</Button>
-                                    </Stack>
-                                </Stack>
+                                <Grid container spacing={2} component='form' onSubmit={onSubmit}>
+                                    <Grid item xs={12} md={6}>
+                                        <TextField
+                                            fullWidth 
+                                            label='Code'
+                                            name='code'
+                                            defaultValue={staging?.code}
+                                            required
+                                            helperText={!!errors?.code && errors?.code[0]}
+                                            error={!!errors?.code}
+                                        /> 
+                                    </Grid>
+                                    <Grid item xs={12} md={6}>
+                                        <TextField
+                                            fullWidth 
+                                            label='Name'
+                                            name='category'
+                                            defaultValue={staging?.category}
+                                            required
+                                            helperText={!!errors?.category && errors?.category[0]}
+                                            error={!!errors?.category}
+                                        /> 
+                                    </Grid>
+                                    <Grid item xs={12} md={6}>
+                                        <TextField
+                                            fullWidth 
+                                            label='Brand'
+                                            name='brand'
+                                            defaultValue={staging?.brand || ''}
+                                            required
+                                            helperText={!!errors?.brand && errors?.brand[0]}
+                                            error={!!errors?.brand}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12} md={6}>
+                                        <TextField
+                                            fullWidth 
+                                            label='Size'
+                                        /> 
+                                    </Grid>
+                                    <Grid item xs={12} md={6}>
+                                        <TextField
+                                            disabled={loadingCategory}
+                                            fullWidth 
+                                            label='Category'
+                                            select
+                                            name='category'
+                                            value={category}
+                                            onChange={handleCategory}
+                                            required
+                                            helperText={!!errors?.category && errors?.category[0]}
+                                            error={!!errors?.category}
+                                        >
+                                            {renderInputParentCategory()}
+                                        </TextField> 
+                                    </Grid>
+                                    <Grid item xs={12} md={6}>
+                                        <TextField
+                                            disabled={category === ''}
+                                            fullWidth 
+                                            label='Sub Category'
+                                            select
+                                            name='sub_category'
+                                            defaultValue={staging?.sub_category || ''}
+                                            required
+                                            helperText={!!errors?.sub_category && errors?.sub_category[0]}
+                                            error={!!errors?.sub_category}
+                                        >
+                                            {renderInputChildCategory()}
+                                        </TextField> 
+                                    </Grid>
+                                    <Grid item xs={12} md={6}>
+                                        <TextField
+                                            fullWidth 
+                                            label='Unit'
+                                            select
+                                            name='unit'
+                                            defaultValue={staging?.unit || ''}
+                                            required
+                                            helperText={!!errors?.unit && errors?.unit[0]}
+                                            error={!!errors?.unit}
+                                        >
+                                            <MenuItem value=''>None</MenuItem>
+                                            <MenuItem value='kg'>KG</MenuItem>
+                                            <MenuItem value='grm'>GRM</MenuItem>
+                                            <MenuItem value='tin'>TIN</MenuItem>
+                                            <MenuItem value='btl'>BTL</MenuItem>
+                                            <MenuItem value='btl'>LTR</MenuItem>
+                                            <MenuItem value='btl'>TUB</MenuItem>
+                                            <MenuItem value='btl'>BAG</MenuItem>
+                                            <MenuItem value='btl'>EA</MenuItem>
+                                            <MenuItem value='btl'>BOX</MenuItem>
+                                            <MenuItem value='btl'>CTN</MenuItem>
+                                            <MenuItem value='btl'>GLN</MenuItem>
+                                            <MenuItem value='btl'>ROLL</MenuItem>
+                                            <MenuItem value='btl'>SLOP</MenuItem>
+                                            <MenuItem value='btl'>PPN</MenuItem>
+                                            <MenuItem value='btl'>SISIR</MenuItem>
+                                            <MenuItem value='btl'>LOT</MenuItem>
+                                        </TextField> 
+                                    </Grid>
+                                    <Grid item xs={12} md={6}>
+                                        <TextField
+                                            fullWidth 
+                                            label='Tax'
+                                            select
+                                            name='tax'
+                                            defaultValue={staging?.tax || ''}
+                                            required
+                                            helperText={!!errors?.tax && errors?.tax[0]}
+                                            error={!!errors?.tax}
+                                        >
+                                            <MenuItem value='1'>Yes</MenuItem>
+                                            <MenuItem value='2'>No</MenuItem>
+                                        </TextField> 
+                                    </Grid>
+                                    <Grid item xs={12} md={12}>
+                                        <Stack direction='row' spacing={2}>
+                                            <LoadingButton loading={loadingSave} fullWidth variant='contained' type='submit'>
+                                                Submit
+                                            </LoadingButton>
+                                            <Button variant='outlined' onClick={handleReset} fullWidth>Reset</Button>
+                                        </Stack>
+                                    </Grid>
+                                </Grid>
                             : <Loading />
                             }
                         </Card>
